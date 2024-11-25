02c574acf738495efdbe9b0aeb88a0a00fb82278
arsyux <arsyux@gmail.com>
2024-11-25 17:59:53 +0900

    1장

---
+++ example/src/main/java/chap01_HelloWorld/Sample01_HelloWorld.java
@@ -0,0 +1,10 @@
+package chap01_HelloWorld;
+
+public class Sample01_HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+		System.out.print("Hello, World!\n");
+	}
+
+}
